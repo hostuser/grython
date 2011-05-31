896cfc1322f5f08d0e0bfd6cb70cf3b54c2ea43b
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-05-31 15:21:38 +1200

    Fixed batchjob example.

--- src/main/jython/grisu/examples/batchJob.py
+++ src/main/jython/grisu/examples/batchJob.py
@@ -1,94 +1,67 @@
 '''
 Created on 17/11/2009
 
+This example shows how to create, submit and wait for the execution of a batchjob with input files.
+
+In order to see what is going on behind the scenes, you can monitor $HOME/.grisu.beta/grisu-jython.debug
+
 For documentation on what methods are available for the MultiPartJob object, have a look here:
-https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT/javadoc/index.html?org/vpac/grisu/backend/model/job/MultiPartJob.html
+http://grisu.github.com/grisu/javadoc/grisu/frontend/model/job/BatchJobObject.html 
 and here for a normal job object:
-https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT/javadoc/org/vpac/grisu/frontend/model/job/JobObject.html
+http://grisu.github.com/grisu/javadoc/grisu/frontend/model/job/JobObject.html
 
 @author: Markus Binsteiner
 '''
 
 from grisu.jcommons.constants import Constants
-from grisu.control import ResubmitPolicy, ResubmitPolicy, \
-    DefaultResubmitPolicy
-from grisu.control.exceptions import NoSuchJobException
-from grisu.frontend.control.login import LoginManager, LoginParams
+from grisu.frontend.control.login import LoginManager
 from grisu.frontend.model.job import JobObject, BatchJobObject, \
     JobsException
-from grisu.frontend.view.swing.jobmonitoring.batch import \
-    BatchJobDialog
-from grisu.model import GrisuRegistryManager
 import sys
 import time
 
-si = LoginManager.loginCommandline("ARCS")
+si = LoginManager.loginCommandline("BeSTGRID-DEV")
 
 # how many jobs do we want
-numberOfJobs = 34
+numberOfJobs = 10
 
-# the (unique) name of the multijob
-multiJobName = "34javaResubmitTest";
+# the (unique) name of the batchjob
+batch_job_name = "test_batch";
 
-# delete an (possibly existing) old job with the same name
-try:
-    si.kill(multiJobName, True);
-    
-    status = si.getActionStatus(multiJobName)
-    while not status.isFinished():
-        percentage = status.getCurrentElements() * 100 / status.getTotalElements()
-        print "Deletion "+str(percentage)+"% finished."
-        time.sleep(3)
-
-except:
-    print "No need to kill and clean job " + multiJobName
-    
+   
 
 # to see whats going on we add a simple event listener. Hm. This doesn't seem to work reliably in jython. 
 #SystemOutMultiJobLogger(multiJobName)
 
 # create the multipart job 
-multiPartJob = BatchJobObject(si, multiJobName, "/ARCS/NGAdmin", "java", Constants.NO_VERSION_INDICATOR_STRING);
-
-# if you want to display a gui element to control/monitor the job, use the following line
-BatchJobDialog.open(si, multiJobName)
-
-# not needed anymore. it's the default now
-multiPartJob.addJobProperty(Constants.DISTRIBUTION_METHOD, Constants.DISTRIBUTION_METHOD_EQUAL);
+batch_job = BatchJobObject(si, batch_job_name, "/nz/nesi", "UnixCommands", Constants.NO_VERSION_INDICATOR_STRING);
 
 # now we can calculate the relative path (from every job directory) to the common input file folder
-pathToInputFiles = multiPartJob.pathToInputFiles()
+pathToInputFiles = batch_job.pathToInputFiles()
 
 for i in range(0, numberOfJobs):
-    # create a unique jobname for every job
-    jobname = multiJobName+"_"+ str(i)
+
     # create the single job
     job = JobObject(si)
-    #job.setSubmissionLocation("")
-    job.setJobname(jobname)
     # better to set the application to use explicitely because in that case we don't need to use mds (faster)
-    job.setApplication("java")
-    job.setCommandline("java -version")
-    # setting the commandline. In this example we reference to both types of possible input files
-    #job.setCommandline("java -jar "+pathToInputFiles+"/JavaTestJob.jar 30 1")
+    job.setCommandline('cat ' + pathToInputFiles+'commonJobFile.txt ' + 'singleJobFile.txt')
     # adding a job-specific input file
-    #job.addInputFileUrl("/home/markus/test/singleJobFile.txt")
-    # the walltime for the job. Can be set individually or for the multijob (which would have to be done later)
-    job.setWalltimeInSeconds(60)
+    job.addInputFileUrl("/home/markus/tmp/singleJobFile.txt")
     # adding the job to the multijob
-    multiPartJob.addJob(job)
+    batch_job.addJob(job)
     
-multiPartJob.setDefaultNoCpus(1);
-multiPartJob.setDefaultWalltimeInSeconds(60);   
+# now we are adding a file that can be used by all of the child jobs. it needs to be referenced via the pathToInputFiles() method shown above
+batch_job.addInputFile('/home/markus/tmp/commonJobFile.txt')
+batch_job.setDefaultNoCpus(1);
+batch_job.setDefaultWalltimeInSeconds(60);   
     
-# now we add an input file that is common to all jobs
-#multiPartJob.addInputFile("/home/markus/sample input files/JavaTestJob.jar");
-# we don't want to submit to tpac because it doesn't work
-#multiPartJob.setSitesToExclude(["uq", "hpsc", "auckland", "canterbury"]);
     
 try:
     print "Creating jobs on the backend and staging files..."
-    multiPartJob.prepareAndCreateJobs(True)
+    # by specifying "True" we tell the backend to automatically distribute the jobs to all available submission locations
+    # this can be finetuned by exluding or including sites. another option would be to specifying the submission location 
+    # for every single job and setting "False" below (this would make job submission faster since jobs don't need to be re-distributed/moved on the backend).
+    batch_job.prepareAndCreateJobs(True)
 except (JobsException), error:
     for job in error.getFailures().keySet():
         print "Job: "+job.getJobname()+", Error: "+error.getFailures().get(job).getLocalizedMessage()
@@ -96,55 +69,21 @@ except (JobsException), error:
     sys.exit()
 
 print "Job distribution:"
-for subLoc in multiPartJob.getOptimizationResult().keySet():
-    print subLoc + " : " +multiPartJob.getOptimizationResult().get(subLoc)
+print batch_job.getOptimizationResult()
 
 
 print "Submitting jobs..."
-multiPartJob.submit()
+batch_job.submit()
 
-restarted = False
 
-# now we wait for all jobs to finish. Actually, we probably should test whether the job was successful as well...
-while not multiPartJob.isFinished(True):
-    # printing some stats
-    print multiPartJob.getProgress()
-    
-    # restart failed jobs everytime
-    failedpolicy = DefaultResubmitPolicy()
-    # to only resubmit failed jobs, we have to remove the waiting jobs resubmission that is set by default
-    multiPartJob.restart(failedpolicy, True)
+# now we wait for all jobs to be finished, checking for updates every 10 seconds. in real life we would set a much higher check intervall since we don't want to overload
+# the backend and also it's not really necessary
+batch_job.waitForJobToFinish(10)
 
-    # restart once after the jobsubmission is finished to optimize job distributions to queues where the job actually runs
-    if not restarted:
-        
-        # actually, it probably would be a good idea to refresh the job status here because otherwise the restart will just 
-        # restart failed jobs that were already submitted with the restart above...  not really sure...
-        #multiPartJob.refresh()
-        
-        # this might not work the first few times because in the background the batchjob is still submitting...
-        print "trying to restarting job..."
-        
-        policy = DefaultResubmitPolicy()
-        # the next line doesn't make sense since it's the default anyway. Just to demonstrate.
-        policy.setProperty(DefaultResubmitPolicy.RESTART_WAITING_JOBS, True)
-        restarted = multiPartJob.restart(policy, True)
-
-        if restarted:
-            print "Job distribution for restarted jobs:"
-            for subLoc in multiPartJob.getOptimizationResult().keySet():
-                resubmitted = True
-                print subLoc + " : " +multiPartJob.getOptimizationResult().get(subLoc)
-        else:
-            print "Job not restarted (yet)."
-    
-    print "Job not finished yet. Waiting..."
-    time.sleep(3)
-
-print "Multipartjob "+multiPartJob.getBatchJobname()+" finished."
+print "BatchJob "+batch_job.getJobname()+" finished."
 
 # finally, everything is ready. We could do a lot more here, but you get the idea...
-for job in multiPartJob.getJobs():
+for job in batch_job.getJobs():
     print "Job: "+job.getJobname()+", Status: "+job.getStatusString(False)
     print
     print "Stdout: "
